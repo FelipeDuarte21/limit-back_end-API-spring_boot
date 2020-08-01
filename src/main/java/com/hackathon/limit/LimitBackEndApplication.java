39e9cc2eaa7f1cc56cf2108ba114ace9cef48930
Felipe Duarte <felipe15lfde@gmail.com>
2020-08-01 14:45:25 -0300

    Subindo a aplicação

---
+++ src/main/java/com/hackathon/limit/LimitBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.limit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimitBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LimitBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hackathon/limit/LimitBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathon.limit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimitBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
